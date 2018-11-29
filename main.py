811790e1c667f3e461c5e88b6d96a5b59c748492
Drew Barrett <dfb1998@gmail.com>
2018-11-29 15:41:11 -0500

    make_parcel

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def pstring(instr):
     toret = instr.encode("utf8")
     return chr(len(toret)).encode("utf8") + toret
 
+def make_parcel(msg):
+    return chr(len(msg)).encode("utf8") + msg
+
 def bindRequest(v, name, passwd):
     return chr(0x23).encode("utf8") + chr(v).encode("utf8") + \
         pstring(name) + pstring(passwd)
@@ -14,3 +17,5 @@ if __name__ == "__main__":
         == b'0a74657374737472696e67'
     assert binascii.hexlify(bindRequest(1, "user", "pass")) \
         == b'230104757365720470617373'
+    assert binascii.hexlify(make_parcel(bindRequest(1, "user", "pass"))) \
+        == b'0c230104757365720470617373'
