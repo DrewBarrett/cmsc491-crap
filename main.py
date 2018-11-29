8cafee018c6ad1ac5c727e595028251b4fdc5872
Drew Barrett <dfb1998@gmail.com>
2018-11-29 15:21:52 -0500

    pstring and bindrequest

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def pstring(instr):
+    toret = instr.encode("utf8")
+    return chr(len(toret)).encode("utf8") + toret
+
+def bindRequest(v, name, passwd):
+    return chr(0x23).encode("utf8") + chr(v).encode("utf8") + \
+        pstring(name) + pstring(passwd)
+
+if __name__ == "__main__":
+    import binascii
+    assert binascii.hexlify(pstring("teststring")) \
+        == b'0a74657374737472696e67'
+    assert binascii.hexlify(bindRequest(1, "user", "pass")) \
+        == b'230104757365720470617373'
